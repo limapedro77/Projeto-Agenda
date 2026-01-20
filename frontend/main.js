261f12d78618017823b1801667a80a5d1212ab76
Pedro Lima <32841pedro@gmail.com>
2026-01-20 10:12:38 -0300

    apagando pasta frontend

--- frontend/main.js
+++
@@ -1,6 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-
-console.log('Olá mundo 3');
-// Removido import do CSS para usar apenas Bootstrap
-// import './assets/css/style.css';
